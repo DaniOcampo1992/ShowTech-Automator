e10dab4be1e026345e020461fd478127d135dc0f
Sergio Ocampos <daniocampo1992@hotmail.com>
2020-05-26 22:55:56 -0400

    Actual progress

--- showtech.py
+++ showtech.py
@@ -1,10 +1,28 @@
-from netmiko import ConnectHandler
-from getpass import getpass
+from netmiko import ConnectHandler  #Used to SSH connect into devices
+from getpass import getpass         #
 
 filename=input("Device list filename?\n")
 
-with open(filename) as f:
-    devicenames = f.read().splitlines()
+while(True):
+    try:
+        with open(filename) as f:
+            devicenames = f.read().splitlines()
+            break
+    except(FileNotFoundError):
+        print("The filename specified does not exist.\n")
+        filename=input("Device list filename?\n")
 
+
+print(devicenames)  #Only for debugging purposes
 username=input("Enter your SSH username: ")
-password=getpass()
+
+while(True):
+    password1=getpass()
+    print("Enter your password again: ")
+    password2=getpass()
+    if (password1!=password2):
+        print("Passwords does not match, type again: ")
+        continue
+    break
+
+print(password1+" "+password2)  #Only for debugging purposes
