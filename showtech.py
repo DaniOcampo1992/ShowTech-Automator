b3a9a67b8c806e56fbe1e58195c6500e73f66b0b
Sergio Ocampos <daniocampo1992@hotmail.com>
2020-05-26 22:24:01 -0400

    Third commit

--- showtech.py
+++ showtech.py
@@ -1,2 +1,10 @@
 from netmiko import ConnectHandler
+from getpass import getpass
 
+filename=input("Device list filename?\n")
+
+with open(filename) as f:
+    commands_to_send = f.read().splitlines()
+
+username=input("Enter your SSH username: ")
+password=getpass()
